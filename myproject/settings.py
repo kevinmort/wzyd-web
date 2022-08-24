721da97b2b726c9912cddb858b1031d4afbbb073
jiweijian <techwe@qq.com>
2022-08-24 15:30:33 +0800

    add localhost.localdomain to ALLOWED_APPS

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@2tcr%p+je0h#+ujlqu*s%ax!$s2%^w9rh%=eg2jv270o3p0+b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost.localdomain']
 
 
 # Application definition
